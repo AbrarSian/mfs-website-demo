5848ae459a9839ee83b53c6ce854d805a55d7322
Abrar Sian <abrarsian96@gmail.com>
2024-09-19 13:12:16 +0600

    add money part js done

--- js folders/home-page.js
+++ js folders/home-page.js
@@ -1,3 +1,17 @@
 document.getElementById("btn-main-1").addEventListener('click', function(event){
     event.preventDefault();
+    const amountIn= document.getElementById('amount-in').value ;
+    const passCheck= document.getElementById('pass-1').value ;
+    const balance= document.getElementById('balance').value ;
+
+    if(passCheck==="1234"){
+        alert("Money has moved into your account");
+        const balanceValue = parseFloat(balance);
+        const amountValue = parseFloat(amountIn);
+        const finalBalance = balanceValue + amountValue;
+        document.getElementById('balance').value = finalBalance ;
+    }
+    else{
+        alert("Incorrect Pin. Please try again");
+    }
 })
